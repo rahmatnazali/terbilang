345a27e6c1f3664b1fea303fcbb2d42c80a3e52d
Rahmat Nazali Salimi <rahmatnazali95@gmail.com>
2023-01-05 09:05:59 +0700

    update test name

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -6,41 +6,41 @@ fn test_zero() {
 }
 
 #[test]
-fn test_basic_number_1() {
+fn test_basic_number_8() {
     assert_eq!(terbilang::from(8), String::from("delapan"));
 }
 
 #[test]
-fn test_basic_number_2() {
+fn test_basic_number_10() {
     assert_eq!(terbilang::from(10), String::from("sepuluh"));
 }
 
 #[test]
-fn test_basic_number_3() {
+fn test_basic_number_11() {
     assert_eq!(terbilang::from(11), String::from("sebelas"));
 }
 
 #[test]
-fn test_teens_1() {
+fn test_teens_12() {
     assert_eq!(terbilang::from(12), String::from("dua belas"));
 }
 
 #[test]
-fn test_teens_2() {
+fn test_teens_13() {
     assert_eq!(terbilang::from(13), String::from("tiga belas"));
 }
 
 #[test]
-fn test_teens_3() {
+fn test_teens_19() {
     assert_eq!(terbilang::from(19), String::from("sembilan belas"));
 }
 
 #[test]
-fn test_tens_1() {
+fn test_tens_20() {
     assert_eq!(terbilang::from(20), String::from("dua puluh"));
 }
 
 #[test]
-fn test_tens_2() {
+fn test_tens_21() {
     assert_eq!(terbilang::from(21), String::from("dua puluh satu"));
 }
